429e2939afdd554327c02688785c1572ce8c2ab1
jack <ronan.hym@gmail.com>
2026-07-07 15:02:53 +0200

    comment and clean parsing_utils.ml

--- ocaml/dttxt/parsing_utils.ml
+++ ocaml/dttxt/parsing_utils.ml
@@ -1,8 +1,8 @@
 
-(* --------------- type definitions --------------- *)
+(* --------------- TYPE DEFINITIONS --------------- *)
 
 
-(* Type d'une liste de features parsée dans la lecture d'un fichier dttxt.*)
+(* Features list type obtained after reading dttxt file. *)
 type parsed_features = parsed_feature list
 and parsed_feature =
 | ParsedBoolFeature
@@ -14,13 +14,16 @@ and parsed_feature =
 ;;
 
 
+(* Type of the value given in a node to compare to the vector value.
+example : threshold for float features, () for bool features, list of enum values for enum features. *)
 type parsed_value =
   | ParsedNullValue
   | ParsedFloatValue of float
   | ParsedEnumValue of string list
 ;;
 
-(* Type d'un arbre parsé à la lecture d'un fichier dttxt.*)
+(* Tree type obtained after reading dttxt file. 
+A node can reference its feature by its string name, or by its int index. *)
 type named_parsed_tree = named_parsed_tree_element list
 and named_parsed_tree_element =
   | ParsedLeaf_ of int                        (* class_number *)
@@ -28,14 +31,15 @@ and named_parsed_tree_element =
   | NamedParsedNode_ of string * parsed_value (* feature_name, threshold *)
 ;;
 
-(* Type d'un arbre parsé à la lecture d'un fichier dttxt.*)
+(* Tree type obtained after reading dttxt file. 
+All nodes reference its feature by its int index. *)
 type parsed_tree = parsed_tree_element list
 and parsed_tree_element =
   | ParsedLeaf of int (* class number *)
   | ParsedNode of int * parsed_value (* indice_feature, threshold *)
 ;;
 
-(* Type d'un vecteur parsé dans la lecture d'un fichier dttxt.*)
+(* Vector type obtained after reading dttxt file. *)
 type parsed_vector = parsed_vector_element list
 and parsed_vector_element =
 | ParsedBoolVectorElement of bool
@@ -48,7 +52,7 @@ type temp_parsed_file = parsed_features * named_parsed_tree * parsed_vector;;
 type parsed_file = parsed_features * parsed_tree * parsed_vector;;
 
 
-
+(* Get the index of a features given its name. If the name is not found, raise Failure. *)
 let get_feature_index name parsed_features =
   
   let rec aux name parsed_features cpt = match parsed_features with
@@ -65,6 +69,7 @@ let get_feature_index name parsed_features =
   in aux name parsed_features 0
 ;;
 
+(* Transform a named_parsed_tree in a parsed_tree (by finding the indices of named features). *)
 let rec unname_tree dt parsed_features = match dt with
   | [] -> []
   | ParsedLeaf_(e) :: q -> ParsedLeaf(e) :: (unname_tree q parsed_features)
@@ -74,7 +79,7 @@ let rec unname_tree dt parsed_features = match dt with
 
 
 
-
+(* Get the feature at a given index in parsed_features. If index out of bounds, raise Failure. *)
 let get_feature_at_index index parsed_features =
   
   let rec aux index parsed_features cpt = match parsed_features with
@@ -84,21 +89,30 @@ let get_feature_at_index index parsed_features =
   in aux index parsed_features 0
 ;;
 
+(* Test if every element of l1 is in l2. *)
 let is_subset l1 l2 = List.fold_right (fun t acc -> (List.mem t l2) && acc) l1 true;;
 
-let rec type_verif dt parsed_features = match dt with
-  | [] -> true
-  | ParsedLeaf(e) :: q -> (type_verif q parsed_features)
-  | ParsedNode(i, v) :: q -> 
-      match get_feature_at_index i parsed_features, v with
-      | ParsedBoolFeature, ParsedNullValue
-      | ParsedNamedBoolFeature(_), ParsedNullValue
-      | ParsedFloatFeature, ParsedFloatValue(_)
-      | ParsedNamedFloatFeature(_), ParsedFloatValue(_) -> true
-      | ParsedEnumFeature(ss1), ParsedEnumValue(ss2)
-      | ParsedNamedEnumFeature(_, ss1), ParsedEnumValue(ss2) -> is_subset ss2 ss1
-      | _ -> false
-       && (type_verif q parsed_features)
+(* Check the types of dt given parsed_feaetures.
+return 
+- None if no type error detected
+- Some i with i the index of the first element of dt where error founded *)
+let type_error dt parsed_features =
+  let rec aux dt parsed_features cpt = match dt with
+    | [] -> None
+    | ParsedLeaf(e) :: q -> (aux q parsed_features (cpt+1))
+    | ParsedNode(i, v) :: q -> 
+        let f = (get_feature_at_index i parsed_features) in
+        match f, v with
+        | ParsedBoolFeature, ParsedNullValue
+        | ParsedNamedBoolFeature(_), ParsedNullValue
+        | ParsedFloatFeature, ParsedFloatValue(_)
+        | ParsedNamedFloatFeature(_), ParsedFloatValue(_) -> aux q parsed_features (cpt+1)
+        | ParsedEnumFeature(ss1), ParsedEnumValue(ss2)
+        | ParsedNamedEnumFeature(_, ss1), ParsedEnumValue(ss2) -> 
+            if not (is_subset ss2 ss1) then Some( cpt )
+            else aux q parsed_features (cpt+1)
+        | _ -> Some( cpt )
+  in aux dt parsed_features 0
 ;;
 
 
@@ -141,24 +155,7 @@ let rec type_verif dt parsed_features = match dt with
 
 
 
-
-
-(* --------------- examples --------------- *)
-(* let v1 = [ParsedBoolVectorElement(true); ParsedFloatVectorElement(0.1)];;
-let t1 = [
-  ParsedNode(0, ParsedNullValue, 1, 4);
-  ParsedNode(1, ParsedFloatValue(0.5), 2, 3);
-  ParsedLeaf(0);
-  ParsedLeaf(1);
-  ParsedLeaf(2)
-];;
-let f1 = t1, v1;; *)
-(* --------------- end of examples --------------- *)
-
-
-
-
-(* --------------- print functions --------------- *)
+(* --------------- PRINT FUNCTIONS --------------- *)
 
 (* for features : *)
 
@@ -238,41 +235,3 @@ let print_tree t = print_endline (string_of_tree t);;
 (* --------------- end of print functions --------------- *)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(* 
-return 
-- None if no type error detected
-- Some i with i the index of the first element of dt where error founded *)
-let type_error dt parsed_features =
-  let rec aux dt parsed_features cpt = match dt with
-    | [] -> None
-    | ParsedLeaf(e) :: q -> (aux q parsed_features (cpt+1))
-    | ParsedNode(i, v) :: q -> 
-        let f = (get_feature_at_index i parsed_features) in
-        (* print_endline ("feature index : " ^ (string_of_int i));
-        print_endline ("feature : " ^ (string_of_feature_element f)); *)
-        match f, v with
-        | ParsedBoolFeature, ParsedNullValue
-        | ParsedNamedBoolFeature(_), ParsedNullValue
-        | ParsedFloatFeature, ParsedFloatValue(_)
-        | ParsedNamedFloatFeature(_), ParsedFloatValue(_) -> aux q parsed_features (cpt+1)
-        | ParsedEnumFeature(ss1), ParsedEnumValue(ss2)
-        | ParsedNamedEnumFeature(_, ss1), ParsedEnumValue(ss2) -> 
-            if not (is_subset ss2 ss1) then Some( cpt )
-            else aux q parsed_features (cpt+1)
-        | _ -> Some( cpt )
-  in aux dt parsed_features 0
-;;
