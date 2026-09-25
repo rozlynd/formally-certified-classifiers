0e647fd6018dae06db4cca55db8bdf4b016604fd
rozlynd <rozlyn.df@gmail.com>
2026-09-25 14:42:07 +0200

    Cleanup in Main

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,4 +1,5 @@
 open Arg
+open Format
 
 open Rfxp
 open Driver_file
@@ -12,16 +13,6 @@ open SatDriver
 let as_list (type t_) (module S : FinSet with type t = t_) (e : S.t) =
   let l = S.elements e in
   List.map (fun f -> Extracted.Utils.to_nat S.n f) l
-
-let string_of_features_with_names l parsed_features = 
-  let rec aux acc l =
-    match l with
-    | [] -> acc ^ " ]"
-    | x :: l -> aux (acc ^ ", \"" ^ get_feature_name_at_index x parsed_features ^ "\"") l
-  in
-  match l with
-  | [] -> "[]"
-  | x :: l -> aux ("[ \"" ^ get_feature_name_at_index x parsed_features ^ "\"") l
 
 let string_of_int_list l =
   let rec aux acc l =
@@ -51,87 +42,94 @@ let spec = [
   "-all", Unit (set_mode All),  "Extract all AXps and CXp"
 ]
 
-let log s =
-  if !verbose then print_endline s
+type log_kind = Info | Error
+
+let kind_as_string = function
+  | Info -> "INFO"
+  | Error -> "ERROR"
+
+let log k mssg =
+  if !verbose then
+    eprintf "[%s] %s@." (kind_as_string k) mssg
+
+let info = log Info
+let error = log Error
+
+(* MAIN *)
+
+let report_axp x = printf "AXp: %s@." (string_of_int_list x)
+let report_cxp x = printf "CXp: %s@." (string_of_int_list x)
+  
+let process_input_problem mode (module Input : DTInputProblem) =
+  match mode with
+  | All ->
+      begin
+        let module Solver = MakeSatSolver in
+        let module Iter = MakeIterator (Input.S) (Solver) in
+        let module AXpFind = DtAXpFinder (Input) in
+        let module CXpFind = DtCXpFinder (Input) in
+        let module WCXpCheck = DtWCXpChecker (Input) in
+        let module Enum = MakeEnumerator (Input) (Iter) (WCXpCheck) (CXpFind) (AXpFind) in
+
+        let report_xp x =
+          match x with
+          | Enum.Xp.Coq_isAXp x -> report_axp (as_list (module Input.S) x)
+          | Enum.Xp.Coq_isCXp x -> report_cxp (as_list (module Input.S) x)
+        in
+
+        let rec iter f get record st =
+          let x = get st in
+          match x with
+          | None -> ()
+          | Some y ->
+            begin
+              f y;
+              let next_st = record y st in
+              iter f get record next_st
+            end
+        in
+
+        iter report_xp Enum.get Enum.record Enum.init
+      end
+
+  | AXp ->
+      begin
+        let module Find = DtAXpFinder (Input) in
+
+        let axp = Find.findAXp Input.S.all in
+        let out = as_list (module Input.S) axp in
+        report_axp out
+      end
+
+  | CXp ->
+      begin
+        let module Find = DtCXpFinder (Input) in
+        let module WCXpCheck = DtWCXpChecker (Input) in
+
+        if WCXpCheck.checkWCXp Input.S.all then
+          let cxp = Find.findCXp Input.S.all in
+          let out = as_list (module Input.S) cxp in
+          report_cxp out
+
+        else
+          error "No CXps! (constant model)"
+      end
 
 let main_file mode fname =
-  log ("info : parsing file '" ^ fname ^ "'");
+  info ("Parsing file '" ^ fname ^ "'");
 
-  let write_stdout = print_endline in
-  let report_axp s = write_stdout ("AXp: " ^ s) in
-  let report_cxp s = write_stdout ("CXp: " ^ s) in
-  
   let module D = Driver_file.MakeData (struct let filename = fname end) in
   let module FTD = MakeFeatureTreeData (D) in
   let module MakeI = MakeDTInputProblem (FTD) in
 
   let process_vector v =
-    write_stdout ("Explaining input = " ^ string_of_vector v);
+    printf "Explaining input = %s@." (string_of_vector v);
     let module Input = MakeI (struct let parsed_vector = v end) in
-    
-    begin
-      match mode with
-      | All ->
-          begin
-            let module Solver = MakeSatSolver in
-            let module Iter = MakeIterator (Input.S) (Solver) in
-            let module AXpFind = DtAXpFinder (Input) in
-            let module CXpFind = DtCXpFinder (Input) in
-            let module WCXpCheck = DtWCXpChecker (Input) in
-            let module Enum = MakeEnumerator (Input) (Iter) (WCXpCheck) (CXpFind) (AXpFind) in
-
-            let report_xp x =
-              match x with
-              | Enum.Xp.Coq_isAXp x -> report_axp (string_of_int_list (as_list (module Input.S) x))
-              | Enum.Xp.Coq_isCXp x -> report_cxp (string_of_int_list (as_list (module Input.S) x))
-            in 
-
-            let rec iter f get record st cpt =
-              let x = get st in
-              print_endline ("iter n°" ^ string_of_int cpt);
-              match x with
-              | None -> ()
-              | Some y ->
-                begin
-                  f y;
-                  let next_st = record y st in
-                  if cpt > 20 then failwith "too much iterations"
-                  else iter f get record next_st (cpt+1)
-                end
-            in
-
-            iter report_xp Enum.get Enum.record Enum.init 0
-          end
-
-      | AXp ->
-          begin
-            let module Find = DtAXpFinder (Input) in
-
-            let axp = Find.findAXp Input.S.all in
-            let out = string_of_features_with_names (as_list (module Input.S) axp) D.features in
-            report_axp out
-          end
-
-      | CXp ->
-          begin
-            let module Find = DtCXpFinder (Input) in
-            let module WCXpCheck = DtWCXpChecker (Input) in
-
-            if WCXpCheck.checkWCXp Input.S.all then
-              let cxp = Find.findCXp Input.S.all in
-              let out = string_of_features_with_names (as_list (module Input.S) cxp) D.features in
-              report_cxp out
-
-            else
-              write_stdout "No CXps! (constant model)"
-          end
-    end;
+    process_input_problem mode (module Input)
   in
 
-  (* run on all vectors *)
   List.iter process_vector D.parsed_vectors;
-  
-  log "info : done"
+  info "Done"
 
 let () =
   parse spec add_fname usage_msg;
